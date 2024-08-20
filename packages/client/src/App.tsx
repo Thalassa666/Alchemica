f892db1328ad9758880ea0b7b18358dbb98e1297
Маргарита Митяговская <margaritamitiagovskaia@MacBook-Air-Margarita.local>
2024-08-21 00:56:30 +0300

    feat(ALC-11) - add router & base paths

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,15 @@
 import { useEffect } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from '@pages/Login'
+import Home from '@pages/Home'
+import Register from '@pages/Register'
+import Profile from '@pages/Profile'
+import Game from '@pages/Game'
+import Leaderboard from '@pages/Leaderboard'
+import Forum from '@pages/Forum'
+import Topic from '@pages/Topic'
+import Error from '@pages/Error'
 
 function App() {
   useEffect(() => {
@@ -12,7 +22,21 @@ function App() {
 
     fetchServerData()
   }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/forum/:topicId" element={<Topic />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
