f7f14de6dcbcc7c71cec9433f24be48f0157c8a1
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2024-08-26 21:25:52 +0300

    fix jest errors

---
+++ packages/client/jest.config.mjs
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import tsconfig from './tsconfig.json' assert { type: 'json' }
+
+dotenv.config()
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
+  transform: {
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
+  },
+  globals: {
+    __SERVER_PORT__: process.env.SERVER_PORT,
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(.*@gravity-ui/uikit.*)/)', // Исключаем @gravity-ui/uikit из игнорирования
+  ],
+}
