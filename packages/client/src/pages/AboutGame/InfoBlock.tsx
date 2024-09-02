46b18357f3bb5dcf4bbd4e08751bcc7e0c117648
krokodila888 <krokodila888@yandex.ru>
2024-09-02 20:51:19 +0300

    FIX: trying to pass tests

--- packages/client/src/pages/AboutGame/InfoBlock.tsx
+++ packages/client/src/pages/AboutGame/InfoBlock.tsx
@@ -1,12 +1,4 @@
 import styles from './styles.module.scss'
-import book from '../../assets/images/book-small-pic.png'
-import skill1 from '../../assets/images/skill1-pic.png'
-import skill2 from '../../assets/images/skill2-pic.png'
-import skill3 from '../../assets/images/skill3-pic.png'
-import potion1 from '../../assets/images/potion1-pic.png'
-import potion2 from '../../assets/images/potion2-pic.png'
-import potion3 from '../../assets/images/potion3-pic.png'
-import arrow from '../../assets/images/arrow.png'
 
 export const InfoBlock = () => {
   return (
