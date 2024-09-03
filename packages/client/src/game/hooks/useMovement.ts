66a70db7943d21931069da61e73fb76398dee0bf
RanelGM <mr.kotenkaA@gmail.com>
2024-09-03 18:14:01 +0300

    feature/ALC-26/add game module for player position and movement

---
+++ packages/client/src/game/hooks/useMovement.ts
@@ -0,0 +1,60 @@
+import { useWindowEffect } from '@core/hooks'
+import { EvtCodes } from '@core/utils/constants'
+import { DirectionType, Position } from '../types/types'
+import { Direction, VelocitySpeed } from '../constants/player'
+import { useGameState } from './useGameState'
+
+/** Использовать движения персонажа */
+export const useMovement = () => {
+  const { getPlayer, updatePlayer } = useGameState()
+
+  /** Изменить движение. Все направления должны использовать эту функцию */
+  const registerMoving = (
+    velocity: Partial<Position>,
+    direction: DirectionType
+  ) => {
+    const updatedVelocity = { ...getPlayer().velocity, ...velocity }
+
+    updatePlayer({ velocity: updatedVelocity, lastDirectionX: direction })
+  }
+
+  /** Движение влево по оси X */
+  const moveLeft = () => {
+    registerMoving({ x: VelocitySpeed * -1 }, Direction.Left)
+  }
+
+  /** Движение вправо по оси X */
+  const moveRight = () => {
+    registerMoving({ x: VelocitySpeed }, Direction.Right)
+  }
+
+  /** Остановиться по оси X */
+  const stopMovingX = () => {
+    registerMoving({ x: 0 }, getPlayer().lastDirectionX)
+  }
+
+  const handleKeydown = (evt: KeyboardEvent) => {
+    const eventsMap = {
+      [EvtCodes.A]: moveLeft,
+      [EvtCodes.D]: moveRight,
+      [EvtCodes.ArrowLeft]: moveLeft,
+      [EvtCodes.ArrowRight]: moveRight,
+    }
+
+    eventsMap[evt.code]?.()
+  }
+
+  const handleKeyup = (evt: KeyboardEvent) => {
+    const eventsMap = {
+      [EvtCodes.A]: stopMovingX,
+      [EvtCodes.D]: stopMovingX,
+      [EvtCodes.ArrowLeft]: stopMovingX,
+      [EvtCodes.ArrowRight]: stopMovingX,
+    }
+
+    eventsMap[evt.code]?.()
+  }
+
+  useWindowEffect('keydown', handleKeydown)
+  useWindowEffect('keyup', handleKeyup)
+}
---
+++ packages/client/src/game/hooks/usePlayer.ts
@@ -0,0 +1,62 @@
+import { CanvasContext } from '../types/types'
+import { useMovement } from './useMovement'
+import { useGameState } from './useGameState'
+import { useGameBorders } from './useGameBorders'
+import { useCraftTools } from './useCraftTools'
+
+/** Использовать данные и управление игрока */
+export const usePlayer = () => {
+  useMovement()
+  const { getPlayer, updatePlayer } = useGameState()
+  const borders = useGameBorders()
+  const craftTools = useCraftTools()
+
+  /** Отрисовать положение игрока */
+  const draw = (context: CanvasContext) => {
+    const { position, size } = getPlayer()
+
+    // TODO: Заменить на реальный спрайт
+    context.ctx.fillStyle = 'green'
+    context.ctx.fillRect(position.x, position.y, size.width, size.height)
+  }
+
+  /** Обновить положение игрока */
+  const update = (_context: CanvasContext) => {
+    const { position, velocity, size, canMove } = getPlayer()
+
+    const updPosition = {
+      x: position.x + velocity.x,
+      y: position.y + velocity.y,
+    }
+
+    const updVelocity = {
+      x: velocity.x,
+      y: velocity.y,
+    }
+
+    const border = borders.checkForNearBorder({
+      ...updPosition,
+      ...size,
+    })
+
+    /* Сбросить все ускорения и позицию, если есть коллизия с границей игры */
+    if (border || !canMove) {
+      updVelocity.x = 0
+      updVelocity.y = 0
+      updPosition.x = position.x
+      updPosition.y = position.y
+    }
+
+    craftTools.checkInteraction({ ...updPosition, ...size })
+
+    updatePlayer({
+      position: updPosition,
+      velocity: updVelocity,
+    })
+  }
+
+  return {
+    draw,
+    update,
+  }
+}
