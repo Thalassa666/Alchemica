b1ad6a09e414b0857c93453c41f428c5392f599b
Маргарита Митяговская <margaritamitiagovskaia@MacBook-Air-Margarita.local>
2024-09-18 22:32:48 +0300

    fix errors tests

--- packages/client/src/game/ui/GameClient.test.tsx
+++ packages/client/src/game/ui/GameClient.test.tsx
@@ -1,11 +1,16 @@
 import '@testing-library/jest-dom'
-import { render, fireEvent } from '@testing-library/react'
+import 'jest-canvas-mock'
+import { render } from '@testing-library/react'
 import { GameClient } from '@src/game'
 import * as GameHooks from '../hooks'
 
+const mockGetStatistic = jest.fn(() => ({
+  endedAt: null,
+}))
+
 jest.mock('../hooks', () => ({
   useGameState: jest.fn(() => ({
-    getStatistic: jest.fn(),
+    getStatistic: mockGetStatistic,
     resetGameState: jest.fn(),
   })),
   useWinLoose: jest.fn(() => ({
@@ -14,24 +19,48 @@ jest.mock('../hooks', () => ({
   useWinConditionDialog: jest.fn(() => ({
     draw: jest.fn(),
   })),
-  useQuickTools: jest.fn(),
-  useCraftTools: jest.fn(),
-  useGameBorders: jest.fn(),
-  usePlayer: jest.fn(),
-  useCraftDialog: jest.fn(),
-  useNotifications: jest.fn(),
-  useReceiptsBook: jest.fn(),
-  useControlsDialog: jest.fn(),
-  useImage: jest.fn(),
-  useMouseInteraction: jest.fn(),
-  useFullScreen: jest.fn(),
+  useQuickTools: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useCraftTools: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useGameBorders: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  usePlayer: jest.fn(() => ({
+    draw: jest.fn(),
+    update: jest.fn(),
+  })),
+  useCraftDialog: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useNotifications: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useReceiptsBook: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useControlsDialog: jest.fn(() => ({
+    draw: jest.fn(),
+  })),
+  useImage: jest.fn(() => ({
+    draw: jest.fn(),
+    drawOverlayFullSize: jest.fn(),
+    drawOverlayByLocation: jest.fn(),
+  })),
+  useMouseInteraction: jest.fn(() => ({
+    init: jest.fn(),
+    destroy: jest.fn(),
+  })),
+  useFullScreen: jest.fn(() => ({
+    init: jest.fn(),
+    destroy: jest.fn(),
+    draw: jest.fn(),
+  })),
 }))
 
 describe('GameClient', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
   test('should render canvas with correct dimensions', () => {
     render(<GameClient onGameEnd={jest.fn()} />)
 
